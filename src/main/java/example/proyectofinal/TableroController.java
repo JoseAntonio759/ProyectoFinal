4ead232f99f597f497184fabb0b7fc42efd29b10
migueeldr <miguel.diegor@edu.uah.es>
2025-05-05 16:56:05 +0200

    Pantalla Tablero

--- src/main/java/example/proyectofinal/TableroController.java
+++ src/main/java/example/proyectofinal/TableroController.java
@@ -1,10 +1,14 @@
 package example.proyectofinal;
 
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.Spinner;
 import javafx.stage.Stage;
 
@@ -17,15 +21,27 @@ public class TableroController {
     private Button volveratras;
     @FXML
     private Button siguiente;
+    @FXML
+    private Slider ancho;
+    @FXML
+    private Slider largo;
+    @FXML
+    private Label valorAncho;
+    @FXML
+    private Label valorLargo;
 
-
+    protected IntegerProperty medidaAncho = new SimpleIntegerProperty(25);
+    protected IntegerProperty medidaLargo = new SimpleIntegerProperty(25);
 
 
 
 
     @FXML
     protected void initialize() {
-
+        ancho.valueProperty().bindBidirectional(medidaAncho);
+        largo.valueProperty().bindBidirectional(medidaLargo);
+        valorAncho.textProperty().bind(medidaAncho.asString());
+        valorLargo.textProperty().bind(medidaLargo.asString());
     }
 
 
